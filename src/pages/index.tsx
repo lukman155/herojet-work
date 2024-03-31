9a1ffc8ff772c757254f270908d07b1d6eff3722
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2024-03-31 15:56:24 +0100

    yalla

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import About from '../components/About';
-import Analytics from '../components/Analytics';
 import Canvas from '../components/Canvas';
 import Features from '../components/Features';
 import Header from '../components/Header';
@@ -43,7 +42,6 @@ const App = () => {
           <About />
         </>
       </LazyShow>
-      <Analytics />
     </div>
   );
 };
