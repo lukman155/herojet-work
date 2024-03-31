84f77bdd15e2e8f831038a048ac031aad5a9f491
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2024-03-31 15:30:14 +0100

    Setup

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,7 @@
+export const AppConfig = {
+  site_name: 'Herojatgeo',
+  title: 'Herojatgeo - Geological Solutions',
+  description:
+    'Your premier destination for cutting-edge geosciences, laboratory, and engineering materials.',
+  locale: 'en',
+};
